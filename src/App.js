f890522a5a5c53844d6a088488f11de47a558ed5
vinh <peakylower@gmail.com>
2025-04-04 00:06:00 +0700

    ud

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import HillCipher from "./pages/HillCipher";
 import Login from "./components/login/Login";
 import Signup from "./components/signup/Signup";
 import Profile from "./components/users/Profile";
+import Docs from "./pages/Docs";
+import Abouts from "./pages/About";
 
 function App() {
   return (
@@ -14,6 +16,9 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/hillcipher" element={<HillCipher />} />
+        <Route path="/docs" element={<Docs />} />
+        <Route path="/abouts" element={<Abouts />} />
+
       </Routes>
     </Router>
   );
---
+++ src/pages/About.jsx
@@ -0,0 +1,7 @@
+const Abouts =() =>{
+    
+
+
+}
+
+export default Abouts;
