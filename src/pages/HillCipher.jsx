45ed02d30085cc7ceb7f87ef99b87eadd06d9b11
trai-tim <cn679810@gmail.com>
2025-04-05 23:17:15 +0700

    fix login

--- src/pages/HillCipher.jsx
+++ src/pages/HillCipher.jsx
@@ -6,28 +6,38 @@ import Footer from "../layout/Footer";
 const HillCipher = () => {
     return (
         <div>
-             <Header />
-        <div className="bg-gray-300 flex flex-col items-center justify-center min-h-screen">
-           
-            <div className="text-center px-4">
-                <p className="text-lg font-bold">Hill cipher</p>
-                <p className="text-base">is a polygraphic substitution cipher that uses linear algebra, specifically matrices, to encrypt and decrypt messages.</p>
-                <p className="text-base mt-2">Invented by <span className="font-bold">Lester S. Hill</span> in 1929, it transforms blocks of plaintext into ciphertext through matrix multiplication and modular arithmetic.</p>
+            <Header />
+            <div className="bg-blackgray text-white flex flex-col items-center justify-center min-h-screen">
+                <div className="text-center px-4">
+                    <p className="text-lg font-bold">Hill cipher</p>
+                    <p className="text-base">
+                        Is a polygraphic substitution cipher that uses linear algebra, specifically matrices, to encrypt and decrypt messages.
+                    </p>
+                    <p className="text-base mt-2">
+                        Invented by <span className="font-bold">Lester S. Hill</span> in 1929, it transforms blocks of plaintext into ciphertext through matrix multiplication and modular arithmetic.
+                    </p>
+                </div>
+
+                <div className="mt-8">
+                    <i className="fas fa-chevron-down text-2xl"></i>
+                </div>
+
+                <div className="mt-4 flex flex-col items-center space-y-2">
+                    <Link
+                        to='/encrypt'
+                        className="bg-gray-500 text-white py-2 px-16 rounded transition duration-300 hover:bg-gray-700 text-center"
+                    >
+                        Mã hóa
+                    </Link>
+                    <Link
+                        to='/decrypt'
+                        className="bg-white text-black py-2 px-4 rounded text-center"
+                    >
+                        Giải mã
+                    </Link>
+                </div>
             </div>
-            <div className="mt-8">
-                <i className="fas fa-chevron-down text-2xl"></i>
-            </div>
-            <div className="mt-4 flex flex-col items-center space-y-2">
-                <button className="bg-gray-500 text-white py-2 px-16 rounded transition duration-300 hover:bg-gray-700">
-                    <Link to='/encrypt'>Mã hóa</Link>
-                </button>
-                <button className="bg-white text-black py-2 px-4 rounded ">
-                    <Link to='/decrypt'>Giải mã</Link>
-                </button>
-            </div>
-           
-        </div>
-        <Footer />
+            <Footer />
         </div>
     );
 };
